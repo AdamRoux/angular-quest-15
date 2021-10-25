1edd75c405e4703729596d3beb0055b67a743cd5
AdamRoux <roux.adam69@gmail.com>
2021-10-25 10:51:39 +0200

    created reactive form

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+
+  userForm = this.fb.group({
+
+  username : [''],
+    credentials: this.fb.group({
+      email : [''],
+      password : [''],
+    }),
+    address: this.fb.group({
+      city: [''],
+      zip: [''],
+      street: [''],
+    
+})
+  })
+
+  // userForm = new FormGroup({
+  // })
+
+  onSubmit() {
+    console.log(this.userForm.value);
+}
+
+  
+  createUser(){
+    
+  }
+  
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+  }
+
+}
